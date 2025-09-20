b5873471a6a6848858e4fdaf4ece5fc5a19b018a
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2025-09-20 16:33:01 +0900

    study(Day-1): custom item, creative tab

---
+++ readme.md
@@ -0,0 +1,26 @@
+### Minecraft forge study
+- version
+    - `minecraft - 1.20.1`
+    - `forge - 47.0.19`
+- reference
+    - [ModdingByKaupenjoe](https://www.youtube.com/@ModdingByKaupenjoe)
+- image
+    - 각 영상의 정보란에 images 를 다운 받을 수 있음.
+
+### Day 1
+<details>
+<summary> Item, CreativeModeTab 추가하기</summary>
+
+`DeferredRegister`는 게임 로딩 시점에 forge 가 알아서 object 를 등록 해준다.  
+이전에는 개발자가 eventListener 에 직접 등록해야 했다.
+
+forge 대략적인 로딩 순서
+1. `mods` 파일을 탐색하여 모든  `.jar` 파일을 읽는다.
+2. `resources/META-INF` 의 `mods.toml` 을 읽어 mod 의 기본 정보를 읽는다.
+3. 이때 `DeferredRegister` 가 생성되며 아직 등록한 object 는 생성되지 않는다.
+4. 모든 mod 가 로딩된 후 `DeferredRegister` 가 활성화 되어 추가한 object 를 생성한다.
+5. 모든 초기화와 등록이 된 후 게임이 실행된다.
+
+위 순서 중 생략한 부분이 있는데 추후 관련된 부분을 개발할 경우 추가할 예정.
+
+</details>
---
+++ src/main/java/zz/dbrvkf/minecraft_study/item/NewItems.java
@@ -0,0 +1,22 @@
+package zz.dbrvkf.minecraft_study.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import zz.dbrvkf.minecraft_study.Minecraft_study;
+
+public class NewItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Minecraft_study.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
